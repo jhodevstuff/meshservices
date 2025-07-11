e0592307a076a4ee4ded9645322fda29cae00a3d
Joshua Hoffmann <github@ijosh.pics>
2025-07-11 10:32:52 +0200

    fixed katwarn

--- meshservices.py
+++ meshservices.py
@@ -32,14 +32,23 @@ def fetch_dwd_warnings():
         dwd_url = "https://warnung.bund.de/bbk.dwd/unwetter.json"
         resp = requests.get(dwd_url, timeout=10)
         data = resp.json()
-        bayern_warnings = []
-        for warn in data:
-            # Bayern
-            if warn.get('stateShort') == 'BY' and warn.get('level', 0) >= 3:
-                bayern_warnings.append(warn)
+        bayern_warnings = [warn for warn in data if warn.get('stateShort') == 'BY' and warn.get('level', 0) >= 3]
         resp2 = requests.get(url, timeout=10)
         data2 = resp2.json()
-        bayern_cat = [w for w in data2 if w.get('stateShort') == 'BY']
+        bayern_cat = []
+        for w in data2:
+            if w.get('stateShort') == 'BY':
+                bayern_cat.append(w)
+            else:
+                info = w.get('info')
+                if isinstance(info, list) and info:
+                    areas = info[0].get('area')
+                    if isinstance(areas, list):
+                        for area in areas:
+                            area_desc = area.get('areaDesc', '')
+                            if 'Bayern' in area_desc or 'BY' in area_desc:
+                                bayern_cat.append(w)
+                                break
         return bayern_warnings, bayern_cat
     except Exception as e:
         print(f"{datetime.now()} - Error fetching warnings: {e}")
@@ -50,9 +59,18 @@ def warn_service(message, nodeid):
     bayern_warnings, bayern_cat = fetch_dwd_warnings()
     meldungen = []
     for w in bayern_warnings:
-        meldungen.append(f"DWD: {w.get('headline', 'Warnung')} (Stufe {w.get('level')}) - {w.get('description', '')}")
+        meldungen.append(f"DWD: {w.get('headline', w.get('event', 'Warnung'))} (Stufe {w.get('level')}) - {w.get('description', '')} [{w.get('stateShort','') or ''}]")
     for w in bayern_cat:
-        meldungen.append(f"Katastrophe: {w.get('headline', 'Warnung')} - {w.get('description', '')}")
+        info = w.get('info')
+        if isinstance(info, list) and info:
+            headline = info[0].get('headline', info[0].get('event', 'Warnung'))
+            desc = info[0].get('description', '')
+            area = ''
+            if 'area' in info[0] and isinstance(info[0]['area'], list) and info[0]['area']:
+                area = info[0]['area'][0].get('areaDesc', '')
+            meldungen.append(f"Katastrophe: {headline} - {desc} [{area}]")
+        else:
+            meldungen.append(f"Katastrophe: Warnung -  []")
     if meldungen:
         send_message_to_node(nodeid, '\n\n'.join(meldungen))
     else:
@@ -67,12 +85,21 @@ def warn_background_loop():
         for w in bayern_warnings:
             wid = w.get('identifier')
             if wid and wid not in warned_ids:
-                meldungen.append(f"DWD: {w.get('headline', 'Warnung')} (Stufe {w.get('level')}) - {w.get('description', '')}")
+                meldungen.append(f"[AUTOWARN] DWD: {w.get('headline', w.get('event', 'Warnung'))} (Stufe {w.get('level')}) - {w.get('description', '')} [{w.get('stateShort','') or ''}]")
                 neue_ids.add(wid)
         for w in bayern_cat:
             wid = w.get('identifier')
             if wid and wid not in warned_ids:
-                meldungen.append(f"Katastrophe: {w.get('headline', 'Warnung')} - {w.get('description', '')}")
+                info = w.get('info')
+                if isinstance(info, list) and info:
+                    headline = info[0].get('headline', info[0].get('event', 'Warnung'))
+                    desc = info[0].get('description', '')
+                    area = ''
+                    if 'area' in info[0] and isinstance(info[0]['area'], list) and info[0]['area']:
+                        area = info[0]['area'][0].get('areaDesc', '')
+                    meldungen.append(f"[AUTOWARN] KAT: {headline} - {desc} [{area}]")
+                else:
+                    meldungen.append(f"[AUTOWARN] KAT: Warnung -  []")
                 neue_ids.add(wid)
         if meldungen:
             for msg in meldungen:
